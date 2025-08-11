11cf991a0c5929dcec4fe21f7cf663674929cc7c
Esakki2 <desakki123@gmail.com>
2025-08-11 13:41:38 +0530

    Fixed home page 404 issue by updating next.config.js

--- next.config.js
+++ next.config.js
@@ -1,18 +1,8 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/home', // or your actual home route if different
-        permanent: true,
-      },
-    ];
-  },
-};
-module.exports = {
   experimental: {
     appDir: true,
   },
-}
+};
+
+module.exports = nextConfig;
